9956552af4e3a954a4bc0796ee1e04f1aad73e9f
Caio Sampaio <39320152+CaioSampaio@users.noreply.github.com>
2022-09-09 19:18:09 -0300

    Teste de geração de thread

--- Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <pthreads.h>
 
-#define NUMBER_OF_THREADS 2
+#define numero_threads 2
 
-void *print_hello_world(void *tid){
+void *imprimirFilosofo(void *tid){
   
-  printf("Hello World. Greetings from threads %d\n", tid);
+  printf("O filósifo %d\n", tid);
   pthred_exit(NULL);
 }
 
 int main(){
   
-  pthreads_t threads[NUMBER_OF_THREADS]
-  int status, i;
+  int status;
+  pthreads_t filosofos[numero_threads]
   
-  for(i=0, i<NUMBER_OF_THREADS; i++){
+  for(int i = 0, i < numero_threads; i++){
     
     print("Main here. Creating thread %d\n", i);
-    status = pthread_creat(&threads[i], NULL, print_hello_world, (void*)i);
+    status = pthread_creat(&filosofos[i], NULL, imprimirFilosofo, (void*)i);
     
     if(status != 0){
-      printf("Ops. pthread_creat returned error code %d\n", status)
-      }
+      printf("Erro ao criar o filsofo %d\n", status)
+    }
   }
   
   return 0;
