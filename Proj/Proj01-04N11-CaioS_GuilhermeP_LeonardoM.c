7aebd231727dd940fd3bc92cba373c7a38ff55ec
Guilherme Picoli <83010918+MrPicoli1@users.noreply.github.com>
2022-09-09 19:05:01 -0300

    Update Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c

--- Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthreads.h>
+
+#define NUMBER_OF_THREADS 10
+
+void *print_hello_world(void *tid){
+  
+  printf("Hello World. Greetings from threads %d\n", tid);
+  pthred_exit(NULL);
+}
+
+
 
 int main(){
-  return 0;
+  
+  pthreads_t threads[NUMBER_OF_THREADS]
+    int status, i;
+  
+  for(i=0, i<NUMBER_OF_THREADS; i++){
+    
+    print("Main here. Creating thread %d\n", i);
+    status = pthread_creat(&threads[i], NULL, print_hello_world, (void*)i);
+    
+    if(status != 0){
+      printf("Ops. pthread_creat returned error code %d\n", status)
+      }
+  }
+  
+  exit(NULL);
 }
 
