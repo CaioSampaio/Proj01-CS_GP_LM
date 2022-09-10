b9c3877940a9cecccc7292c564cfa5dc1aafbaa2
Caio Sampaio <39320152+CaioSampaio@users.noreply.github.com>
2022-09-10 10:32:54 -0300

    Linha de pensamento para o Problema
    No lugar de imprimir, faremos um método que faz o filósofo pensar e comer:
    
    1º ele pensa por um tempo randômico (talvez um contador);
    2º ele tenta pegar dois garfos para comer e, se conseguir, come por um tempo randômico (talvez um contador);
    3º ele libera os garfos;
    
    -> para os garfos: podemos criar um vetor que recebe o id do filósofo. Então ao tentar comer, o filósofo vai tentar colocar o seu id duas vezes no vetor, se conseguir: está comendo; se não: volta a pensar.

--- Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -5,6 +5,51 @@
 
 #define numero_threads 2
 
+/*
+
+int quantidadeGarfos, garfos[] = {0, 0, 0, 0, 0};
+
+void pensar(int i){
+  printf("O filósofo %d está pensado", i);
+  //Timer Randômico
+}
+
+void comer (int i){
+  int garfosNaMao = 0;
+  
+  for (int j = 0; j < quantidadeGarfos; j++){
+    if (garfos[j] == 0){
+      garfos[j] = i;
+      garfosNaMao = garfosNaMao + 1;
+    }
+  }
+  
+  if (garfosNaMao == 2){
+  
+    printf("O filósofo %d está comendo", i);
+  
+    //Timer Randômico
+    
+    for (int j = 0; j < quantidadeGarfos; j++){
+      if (garfos[j] == i){
+        garfos[j] = 0;
+        garfosNaMao = garfosNaMao - 1;
+      }
+    }
+    
+  }
+}
+
+void *acoesNaMesa(void *tid, int i){
+  
+  for (;;){
+    pensar();
+    comer();
+  }
+}
+
+*/
+
 void *imprimirFilosofo(void *tid){
   
   printf("O filósifo %d\n", tid);
@@ -20,6 +65,7 @@ int main(){
     
     print("Main here. Creating thread %d\n", i);
     status = pthread_creat(&filosofos[i], NULL, imprimirFilosofo, (void*)i);
+    // status = pthread_creat(&filosofos[i], NULL, acoesNaMesa, (void*)i);
     
     if(status != 0){
       printf("Erro ao criar o filsofo %d\n", status);
