73131d78e99fd2f5ca78d3b4ed04532c37dbf509
CaioSampaio <sampaio.caiooliveira@gmail.com>
2022-09-10 16:23:12 -0300

    Alteração no Argumento em pthread_creat e acoesNaMesa

--- Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -8,33 +8,33 @@
 int quantidadeGarfos, garfos[] = {0, 0, 0, 0, 0};
 
 void pensar(int i){
-  printf("O filósofo %d está pensado", i);
+  printf("\nO filósofo %d está pensado", i);
   //Timer Randômico
 }
 
 void comer (int i){
   int garfosNaMao = 0;
-  
+
   for (int j = 0; j < quantidadeGarfos; j++){
     if (garfos[j] == 0){
       garfos[j] = i;
       garfosNaMao = garfosNaMao + 1;
     }
   }
-  
+
   if (garfosNaMao == 2){
-  
-    printf("O filósofo %d está comendo", i);
-  
+
+    printf("\nO filósofo %d está comendo", i);
+
     //Timer Randômico
-    
+
     for (int j = 0; j < quantidadeGarfos; j++){
       if (garfos[j] == i){
         garfos[j] = 0;
         garfosNaMao = garfosNaMao - 1;
       }
     }
-    
+
   } else{
      for (int j = 0; j < quantidadeGarfos; j++){
       if (garfos[j] == i){
@@ -45,8 +45,8 @@ void comer (int i){
   }
 }
 
-void *acoesNaMesa(int i){
- // int *i = (int *)arg;
+void *acoesNaMesa(void *arg){
+  int i = (int)arg;
 
   for (;;){
     pensar(i);
@@ -56,27 +56,27 @@ void *acoesNaMesa(int i){
 
 /*
 void *imprimirFilosofo(void* tid){
-  
+
   printf("O filósifo %p\n", tid);
   pthread_exit(NULL);
 }
 */
 
 int main(){
-  
+
   int status;
   pthread_t filosofos[numero_threads];
-  
+
   for(int i = 0; i < numero_threads; i++){
-    
+
     printf("Main here. Creating thread %d\n", i);
     //status = pthread_create(&filosofos[i], NULL, imprimirFilosofo,(void*)(intptr_t)i);
-    status = pthread_create(&filosofos[i], NULL, acoesNaMesa, i);
-    
+    status = pthread_create(&filosofos[i], NULL, acoesNaMesa, (void*)(intptr_t)i);
+
     if(status != 0){
       printf("Erro ao criar o filsofo %d\n", status);
     }
   }
-  
+
   return 0;
 }
