e1bcb7e97800d5c65dfc5cef5eafc0a7d75c00d3
CaioSampaio <sampaio.caiooliveira@gmail.com>
2022-09-14 14:55:42 -0300

    Alteração nos comentários

--- Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj01-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -37,6 +37,7 @@
 
 #define numero_threads 5
 
+//Vetor de garfos: zero = disponível; maior que zero (ID) = reservado pelo filósofo
 int quantidadeGarfos = 5, garfos[] = {0, 0, 0, 0, 0};
 
 /* ----- FUNÇÕES AUXILIARES ----- */
@@ -55,54 +56,59 @@ void pensar(int i){
     printf("\nO filósofo %d está pensado", i+1);
 
     //Timer Fixo de 5 segundos
-    sleep(5);
+    sleep(5); //Filósofo irá pensar por 5 segundos
 }
 
 // Filósofo realiza a ação de tentar comer
 void comer (int i){
-    int garfosNaMao = 0;
+    int garfosNaMao = 0; //Contabilizador de garfos na mão
 
     //Tenta pegar o garfo da direita
     if (garfos[i] == 0){
-        garfos[i] = i;
-        garfosNaMao = garfosNaMao + 1;
+        garfos[i] = i; //Reserva o garfo da direita com o seu ID
+        garfosNaMao = garfosNaMao + 1; //Contabiliza o nº de garfos na mão
     }
 
     //Verifica se é o filósofo 1
     if ((i - 1) == (-1)){
         //Tenta pegar o garfo da esquerda (que pertence ao filósofo 5)
         if (garfos[quantidadeGarfos - 1] == 0){
-            garfos[quantidadeGarfos - 1] = i;
-            garfosNaMao = garfosNaMao + 1;
+            garfos[quantidadeGarfos - 1] = i; //Reserva o garfo da esquerda com o seu id
+            garfosNaMao = garfosNaMao + 1; //Contabiliza o nº de garfos na mão
         }
     } else {
         //Tenta pegar o garfo da esquerda (que pertence a qualquer filósofo)
         if (garfos[i - 1] == 0){
-            garfos[i - 1] = i;
-            garfosNaMao = garfosNaMao + 1;
+            garfos[i - 1] = i; //Reserva o garfo da esquerda com o seu ID
+            garfosNaMao = garfosNaMao + 1; //Contabiliza o nº de garfos na mão
         }
     }
 
+    //Verfica se o filósofo tem dois garfos (o da esquerda e o da direita)
     if (garfosNaMao == 2){
 
         printf("\nO filósofo %d está comendo", i+1);
 
         //Timer Fixo de 5 segundos
-        sleep(5);
+        sleep(5); //Filósofo irá comer por 5 segundos
 
+        //Devolve os garfos à mesa
         for (int j = 0; j < quantidadeGarfos; j++){
+            //Verifica se o garfo está reservado com o ID do filósofo
             if (garfos[j] == i){
-                garfos[j] = 0;
-                garfosNaMao = garfosNaMao - 1;
+                garfos[j] = 0; //Deixa o garfo como disponível (zero)
+                garfosNaMao = garfosNaMao - 1; //Ajusta o contabilizador de garfos
             }
         }
 
     } else{
 
+        //Devolve o garfo à mesa
         for (int j = 0; j < quantidadeGarfos; j++){
+            //Verifica se o garfo está reservado com o ID do filósofo
             if (garfos[j] == i){
-                garfos[j] = 0;
-                garfosNaMao = garfosNaMao - 1;
+                garfos[j] = 0; //Deixa o garfo como disponível (zero)
+                garfosNaMao = garfosNaMao - 1; //Ajusta o contabilizador de garfos
             }
         }
     }
@@ -112,9 +118,10 @@ void comer (int i){
 void *acoesNaMesa(void *arg){
     int i = (intptr_t)arg;
     
+    //Cada filósofo executa as ações de pensar e comer indefinidamente
     for (;;){
-        pensar(i);
-        comer(i);
+        pensar(i); //Primeiro o filósofo pensa
+        comer(i); //Segundo ele tenta comer
     }
 }
 
@@ -126,10 +133,13 @@ int main(){
     int status;
     pthread_t filosofos[numero_threads];
 
+    //Cria os filósofos com threads
     for(int i = 0; i < numero_threads; i++){
 
+        //Cria cada filósofo executando o método acoesNaMesa e salva o status de criação
         status = pthread_create(&filosofos[i], NULL, acoesNaMesa, (void*)(intptr_t)i);
 
+        //Verifica se a criação foi bem-sucedida
         if(status != 0){
             printf("O filsofo recusou-se a sentar na mesa %d\n", status);
         } else{
@@ -137,10 +147,13 @@ int main(){
         }
     }
 
+    //Espera cada thread (filósofo) terminar
     for(int i = 0; i < numero_threads; i++){
 	
+	//Realiza o join de cada filósofo e salva o status da operação
         status = pthread_join(filosofos[i], NULL);
         
+        //Verifica se a operação foi bem-sucedida
         if(status != 0){
             printf("O filsofo morreu à mesa %d\n", status);
         }
